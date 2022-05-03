5f3592cdb7f3ffc22e9d6811caf0b73bcdbfc959
Shunichiro Aki <register.develop@nana4.net>
2022-05-03 16:01:49 +0900

    MACアドレスとIPアドレスにユニークインデックスを追加

--- database/install.sql
+++ database/install.sql
@@ -10,3 +10,6 @@ CREATE TABLE computers(
   mac_address text not null,
   PRIMARY KEY (id)
 );
+
+CREATE UNIQUE INDEX uk_computers_ip_address on computers(ip_address);
+CREATE UNIQUE INDEX uk_computers_mac_address on computers(mac_address);
